07a7d0b08bca500f25ef7ffd06059d4d5d4f974d
sardars270 <sardars270@gmail.com>
2025-05-26 17:29:22 -0300

    v3

--- src/test/MyCLass.java
+++ src/test/MyCLass.java
@@ -5,7 +5,7 @@ public class MyCLass {
 	public static void main(String[] args) {
         System.out.println("Hello, world!");
         System.out.println("new change");
-        System.out.println("version 2");
+        System.out.println("version 3");
         
     }
 
