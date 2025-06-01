ed25ee434bc9417bae7440556b0dad9db1a6e975
sardars270 <sardars270@gmail.com>
2025-06-01 15:07:26 -0300

    changes added to design to push deign into remote

--- src/test/Test1.java
+++ src/test/Test1.java
@@ -5,6 +5,7 @@ public class Test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("design branch added");
+		System.out.println("Changes to push into remote from design branch");
 
 	}
 
